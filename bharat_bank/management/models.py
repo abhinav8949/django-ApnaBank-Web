1a86a58e8205501a39b832155791c74fad6f54b8
abhinav <abhinavday@gmail.com>
2021-05-14 16:39:30 +0530

    first version

---
+++ bharat_bank/management/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from datetime import datetime 
+
+# Create your models here.
+class Customer(models.Model):
+    account_number = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=30,null=False)
+    slug = models.CharField(max_length=30,null=False,unique=True)
+    email = models.EmailField()
+    current_balance = models.FloatField(max_length=20,default=0)
+
+    def __str__(self):
+        return self.name
+
+    def register(self):
+        self.save()
+    
+class TransectionHistory(models.Model):
+    from_account = models.IntegerField(null=False)
+    to_account = models.IntegerField(null=False)
+    amount = models.FloatField(max_length=10,default=0)
+    date_time = models.DateTimeField(default=datetime.now(), blank=True)
+     
